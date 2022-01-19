ddc7f1d483ce007b64422983293c7d7d1e517ea6
ICanOnlySuffer <pierorojas@tutamail.com>
2022-01-19 01:47:33 -0500

    added hash_new function

--- putils/hashmap.h
+++ putils/hashmap.h
@@ -1,5 +1,5 @@
 # ifndef _HASHMAP_
-# define _HASHMAP_ 1
+# define _HASHMAP_ 2
 
 # include "string.h"
 # include "vector.h"
@@ -8,6 +8,15 @@ struct hash {
 	str key;
 	str value;
 };
+
+struct hash * hash_new (str key, str value) {
+	struct hash * hash = malloc (sizeof (struct hash));
+	
+	hash -> key = key;
+	hash -> value = value;
+	
+	return hash;
+}
 
 str hashmap_get (vec *hashmap, str key) {
 	for (u16 i = 0; i < hashmap -> size; i++) {
