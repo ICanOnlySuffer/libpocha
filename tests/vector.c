5a7858a1a74f2242394af647100e9c6494b01df2
ICanOnlySuffer <pierorojas@tutamail.com>
2022-03-16 01:16:41 -0500

    minor changes

--- tests/vector.c
+++ tests/vector.c
@@ -15,10 +15,8 @@ chr main () {
 	vec * names_vector = vec_new (4);
 	
 	for (u08 i = 0; i < n_names; i++) {
-		vec_append (names_vector, &(names [i]));
-		assert_equal_str (
-			*(str *) VEC_LAST (names_vector), names [i]
-		);
+		vec_psh (names_vector, &(names [i]));
+		assert (eql_str, VEC_LAST (names_vector), &(names [i]));
 	}
 }
 
