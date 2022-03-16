5de0b859e6d82a91bb20ba075164080f2c09c2b1
ICanOnlySuffer <pierorojas@tutamail.com>
2022-03-16 01:16:33 -0500

    updated

--- tests/tester.c
+++ tests/tester.c
@@ -10,25 +10,35 @@ nil it (str should) {
 	PUT_ARR ("\e[32mIt ", should, "\e[0m\n");
 }
 
-nil assert_equal_u64 (u64 value_1, u64 value_2) {
-	str_frm_u64 (tester_buffers [1], value_1);
-	str_frm_u64 (tester_buffers [2], value_2);
+u08 eql_u64 (ptr num_1, ptr num_2) {
+	str_frm_u64 (tester_buffers [1], *(u64 *) num_1);
+	str_frm_u64 (tester_buffers [2], *(u64 *) num_2);
 	STR_CPY (
 		tester_buffers [0],
 		tester_buffers [1],
 		" == ",
 		tester_buffers [2]
 	);
-	PUT_ARR (
-		tester_check [value_1 == value_2],
-		tester_buffers [0], "\e[0m\n"
-	);
+	ret *(u64 *) num_1 == *(u64 *) num_2;
 }
 
-nil assert_equal_str (str string_1, str string_2) {
-	PUT_ARR (
-		tester_check [STR_EQL (string_1, string_2)],
-		"\"", string_1, "\" == \"", string_2, "\"\e[0m\n"
+u08 eql_u08 (ptr u08_1, ptr u08_2) {
+	u64 u64_1 = (u64) *(u08 *) u08_1;
+	u64 u64_2 = (u64) *(u08 *) u08_2;
+	ret eql_u64 (&u64_1, &u64_2);
+}
+
+u08 eql_str (ptr str_1, ptr str_2) {
+	STR_CPY (
+		tester_buffers [0],
+		"\"", *(str *) str_1, "\" == \"", *(str *) str_2, "\""
 	);
+	ret STR_EQL (*(str *) str_1, *(str *) str_2);
+}
+
+nil assert (u08 (* fun) (ptr, ptr), ptr val_1, ptr val_2) {
+	put (tester_check [fun (val_1, val_2)]);
+	put (tester_buffers [0]);
+	put ("\e[0m\n");
 }
 
