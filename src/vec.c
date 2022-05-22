999def6439e487e331617e5a32388a1a09575bb8
ICanOnlySuffer <pierorojas@tutamail.com>
2022-05-22 13:07:07 -0500

    added fail conditions

--- src/vec.c
+++ src/vec.c
@@ -1,29 +1,54 @@
+# include "../inc/num.h"
 # include "../inc/vec.h"
 
-nil vec_rsz (vec * vector, u16 capacity) {
+// vector.resize capacity
+u08 vec_rsz (vec * vector, u16 capacity) {
 	ptr * items = realloc (vector -> items, sizeof (ptr) * capacity);
 	if (items) {
 		vector -> capacity = capacity;
 		vector -> items = items;
+		ret true;
 	}
+	ret false;
 }
 
-nil vec_psh (vec * vector, ptr item) {
+// vector.push value
+u08 vec_psh (vec * vector, ptr item) {
 	if (vector -> capacity == vector -> size) {
-		vec_rsz (vector, vector -> capacity * 2);
+		if (not vec_rsz (vector, vector -> capacity * 2)) {
+			ret false;
+		}
 	}
 	vector -> items [vector -> size++] = item;
+	ret true;
 }
 
-nil vec_psh_arr (vec * vector, u08 n_items, ptr items []) {
+// vector.push *values
+u08 vec_psh_arr (vec * vector, u08 n_items, ptr items []) {
 	if (vector -> capacity < vector -> size + n_items) {
-		vec_rsz (vector, vector -> size * 2);
+		u16 new_size = u64_max (n_items, vector -> size) * 2;
+		if (not vec_rsz (vector, new_size)) {
+			ret false;
+		}
 	}
 	while (vector -> size < n_items) {
 		vector -> items [vector -> size++] = *items++;
 	}
+	ret true;
 }
 
+// vector.index value
+u16 vec_idx (vec * vector, ptr pointer) {
+	u16 i;
+	for (i = 0; i < vector -> size; i++) {
+		if (vector -> items [i] == pointer) {
+			break;
+		}
+	}
+	ret i;
+}
+
+// vector.delete_first_at index
 nil vec_rmv_idx (vec * vector, u16 index) {
 	while (index < vector -> size) {
 		vector -> items [index] = vector -> items [index + 1];
@@ -32,27 +57,20 @@ nil vec_rmv_idx (vec * vector, u16 index) {
 	vector -> items [--vector -> size] = NIL;
 }
 
-u16 vec_idx (vec * vector, ptr pointer) {
-	u16 i;
-	for (i = 0; i < vector -> size; i++) {
-		if (vector -> items [i] == pointer) {
-			break;
-		}
+// vector.delete_first value
+u08 vec_rmv (vec * vector, ptr pointer) {
+	u16 index = vec_idx (vector, pointer);
+	if (index == vector -> size) {
+		ret false;
 	}
-	return i;
+	vec_rmv_idx (vector, index);
+	ret true;
 }
 
-nil vec_for_all (vec * vector, nil (* function) (ptr)) {
+// vector.for_each &:proc
+nil vec_for_all (vec * vector, prc proc) {
 	for (u16 i = 0; i < vector -> size; i++) {
-		function (vector -> items [i]);
+		proc (vector -> items [i]);
 	}
-}
-
-vec * vec_new (u16 capacity) {
-	vec * vector = malloc (sizeof (vec));
-	vector -> items = malloc (sizeof (ptr) * capacity);
-	vector -> capacity = capacity;
-	vector -> size = 0;
-	return vector;
 }
 
