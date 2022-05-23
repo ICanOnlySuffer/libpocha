d9ef655da7ecb46c0ff784004de2d4b23079428a
ICanOnlySuffer <pierorojas@tutamail.com>
2022-05-22 22:49:38 -0500

    replaced with generated ver.h

--- src/pul.zsh
+++
@@ -1,6 +0,0 @@
-#!/bin/env sh
-echo "\
-,-, , , ,   Pocha's Utility Library %s
-| | | | |
-|-´ | | |   Copyright (c) 2022 Piero Estéfano Rojas Effio
-'   \`-´ \`-- GNU General Public License v3.0"
---
+++ src/ver.c
@@ -0,0 +1,12 @@
+
+# define PUL_VERSION_MAYOR %s
+# define PUL_VERSION_MINOR %s
+# define PUL_VERSION_PATCH %s
+
+/*
+,-, , , ,   Pocha's Utility Library
+|_' | | |
+|   | | |   Copyright (c) 2022 Piero Estéfano Rojas Effio
+'   '-' '-- GNU General Public License v3.0
+*/
+
