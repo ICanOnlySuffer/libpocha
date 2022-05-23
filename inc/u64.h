f7abd983720e3c199815d22bacb7ee54cb730cf4
ICanOnlySuffer <pierorojas@tutamail.com>
2022-05-22 22:46:20 -0500

    renamed

---
+++ inc/u64.h
@@ -0,0 +1,17 @@
+
+# ifndef PUL_NUM_H
+# define PUL_NUM_H
+
+# include "cor.h"
+
+ext u64 u64_pow_u64 (u64 number_1, u64 number_2);
+ext u64 u64_frm_str (str string);
+inl u64 u64_min (u64 number_1, u64 number_2) {
+	ret number_1 < number_2 ? number_1 : number_2;
+}
+inl u64 u64_max (u64 number_1, u64 number_2) {
+	ret number_1 > number_2 ? number_1 : number_2;
+}
+
+# endif // PUL_NUM_H
+
--- src/u64.c
+++ src/u64.c
@@ -1,19 +1,20 @@
 # include "../inc/num.h"
 
+// number ** power
 u64 u64_pow_u64 (u64 number, u64 power) {
 	u64 out = 1;
 	while (power--) {
 		out *= number;
 	}
-	return out;
 }
 
+// number.to_s
 u64 u64_frm_str (str string) {
 	u64 length = str_len (string);
 	u64 number = 0;
 	while (length--) {
 		number += (*(string++) - 48) * u64_pow_u64 (10, length);
 	}
-	return number;
+	ret number;
 }
 
