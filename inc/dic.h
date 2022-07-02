daffd1c6c02f9e185ed56dc523a31a423256c2b8
ICanOnlySuffer <pierorojas@tutamail.com>
2022-07-02 18:57:02 -0500

    working as intended now

--- inc/dic.h
+++ inc/dic.h
@@ -4,28 +4,53 @@
 
 # include "vec.h"
 # include "str.h"
+# include "num.h"
 
-/*
-NOTES:
-- remember to always do `vec_for_all (dictionary, free)`
-*/
+# define EQL_PRC u08 (*) (u64, u64)
 
 typedef struct {
 	u64 key;
 	ptr value;
 } key_val;
 
-ext u16 dic_idx (vec * dictionary, u64 key);
-ext u16 dic_str_idx (vec * dictionary, str key);
+ext u16 dic_index_of (vec * dic, u64 key, u08 (*eql) (u64, u64));
+inl u16 dic_index_of_num (vec * dic, u64 key) {
+	ret dic_index_of (dic, key, num_equal);
+}
+inl u16 dic_index_of_str (vec * dic, str key) {
+	ret dic_index_of (dic, (u64) key, (EQL_PRC) str_equal);
+}
 
-ext ptr dic_get (vec * dictionary, u64 key);
-ext ptr dic_str_get (vec * dictionary, str key);
+ext ptr dic_get (vec * dic, u64 key, u08 (*eql) (u64, u64));
+inl ptr dic_get_num (vec * dic, u64 key) {
+	ret dic_get (dic, key, num_equal);
+}
+inl ptr dic_get_str (vec * dic, str key) {
+	ret dic_get (dic, (u64) key, (EQL_PRC) str_equal);
+}
 
-ext u08 dic_psh (vec * dictionary, u64 key, ptr value);
-ext u08 dic_rmv (vec * dictionary, u64 key);
+ext u08 dic_push (vec * dic, u64 key, ptr value);
 
-inl u08 dic_inc (vec * dictionary, u64 key) {
-	ret dic_idx (dictionary, key) != dictionary -> size;
+ext u08 dic_remove_at (vec * dic, u64 key, u08 (*eql) (u64, u64));
+inl u08 dic_remove_at_num (vec * dic, u64 key) {
+	ret dic_remove_at (dic, key, num_equal);
+}
+inl u08 dic_remove_at_str (vec * dic, str key) {
+	ret dic_remove_at (dic, (u64) key, (EQL_PRC) str_equal);
+}
+
+inl u08 dic_includes_num (vec * dic, u64 key) {
+	ret dic_index_of_num (dic, key) != dic -> size;
+}
+inl u08 dic_includes_str (vec * dic, str key) {
+	ret dic_index_of_str (dic, key) != dic -> size;
+}
+
+inl vec dic_new (u16 capacity) {
+	ret vec_new (capacity);
+}
+inl nil dic_free (vec * dic) {
+	free (dic -> items);
 }
 
 # endif // LIBPOCHA_DIC_H
--- src/dic.c
+++ src/dic.c
@@ -1,61 +1,43 @@
 # include "../inc/dic.h"
 # include "../inc/str.h"
+# include <stdlib.h>
 
-u16 dic_idx (vec * dictionary, u64 key) {
+u16 dic_index_of (vec * dic, u64 key, u08 (*eql) (u64, u64)) {
 	u16 i;
-	for (i = 0; i < dictionary -> size; i++) {
-		key_val * key_value = dictionary -> items [i];
-		if (key_value -> key == key) {
+	for (i = 0; i < dic -> size; i++) {
+		key_val * key_value = dic -> items [i];
+		if (eql (key_value -> key, key)) {
 			break;
 		}
 	}
 	ret i;
 }
 
-u16 dic_str_idx (vec * dictionary, str key) {
-	u16 i;
-	for (i = 0; i < dictionary -> size; i++) {
-		key_val * key_value = dictionary -> items [i];
-		if (str_eql ((str) key_value -> key, key)) {
-			break;
-		}
-	}
-	ret i;
-}
-
-ptr dic_get (vec * dictionary, u64 key) {
-	u16 index = dic_idx (dictionary, key);
-	if (index == dictionary -> size) {
+ptr dic_get (vec * dic, u64 key, u08 (* eql) (u64, u64)) {
+	u16 index = dic_index_of (dic, key, eql);
+	if (index == dic -> size) {
 		ret NIL;
 	}
-	ret ((key_val *) dictionary -> items [index]) -> value;
+	ret ((key_val *) dic -> items [index]) -> value;
 }
 
-ptr dic_str_get (vec * dictionary, str key) {
-	u16 index = dic_str_idx (dictionary, key);
-	if (index == dictionary -> size) {
-		ret NIL;
-	}
-	ret ((key_val *) dictionary -> items [index]) -> value;
-}
-
-u08 dic_psh (vec * dictionary, u64 key, ptr value) {
+u08 dic_push (vec * dic, u64 key, ptr value) {
 	key_val * key_value = malloc (sizeof (key_val));
 	key_value -> value = value;
 	key_value -> key = key;
-	if (vec_psh (dictionary, key_value)) {
+	if (vec_push (dic, key_value)) {
 		ret true;
 	}
 	free (key_value);
 	ret false;
 }
 
-u08 dic_rmv (vec * dictionary, u64 key) {
-	u16 index = dic_idx (dictionary, key);
-	if (index == dictionary -> size) {
+u08 dic_remove_at (vec * dic, u64 key, u08 (*eql) (u64, u64)) {
+	u16 index = dic_index_of (dic, key, eql);
+	if (index == dic -> size) {
 		ret false;
 	}
-	vec_rmv_idx (dictionary, index);
+	vec_remove_at (dic, index);
 	ret true;
 }
 
