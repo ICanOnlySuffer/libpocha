f32fa212a75d501588c4db4e8000590da096becd
ICanOnlySuffer <pierorojas@tutamail.com>
2022-07-02 22:42:59 -0500

    added safe for_all

--- inc/arr.h
+++ inc/arr.h
@@ -4,7 +4,8 @@
 
 # include "cor.h"
 
-ext nil arr_for_all (u16 length, ptr array [], nil (* proc) (ptr));
+ext nil arr_for_all (u16 size, ptr array [], nil (* proc) (ptr));
+ext u08 arr_for_all_safe (u16 size, ptr array [], u08 (* proc) (ptr));
 
 # endif // LIBPOCHA_ARR_H
 
--- src/arr.c
+++ src/arr.c
@@ -6,3 +6,14 @@ nil arr_for_all (u16 length, ptr array [], nil (* proc) (ptr)) {
 	}
 }
 
+u08 arr_for_all_safe (u16 length, ptr array [], u08 (* proc) (ptr)) {
+	for (u16 i = 0; i < length; i++) {
+		if (not proc (array [i])) {
+			ret false;
+		}
+	}
+	ret true;
+}
+
+
+
